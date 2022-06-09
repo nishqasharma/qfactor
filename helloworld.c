15567e63ae2b57f0cba9a82a9c37806f9707c5df
nishqa <nishqasharmagradapps@gmail.com>
2022-06-09 15:37:44 -0700

    starting gpu test

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
 
+#include<assert.h>
+
+// CUDA runtime
+#include <cuda_runtime.h>
+#include <cuda_profiler_api.h>
+
+// Helper functions and utilities to work with CUDA
+#include <helper_functions.h>
+#include <helper_cuda.h>
+
 int main()
 {
     printf("hello world! welcome to qfactor!\n");
-    printf("now im updating the code on my laptop");
+    printf("now im updating the code on my laptop\n");
     return 0;
 }
