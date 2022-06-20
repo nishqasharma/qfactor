a7df8b0b011d14c9b8d3410788bd8401ac1cafda
nishqa <nishqasharmagradapps@gmail.com>
2022-06-20 09:28:11 -0700

    starting code

--- tensor_contraction.cu
+++ tensor_contraction.cu
@@ -169,7 +169,7 @@ int main(int argc, char** argv)
   // ==============================
 
   // Initialize cuTENSOR library
-  cutensorHandle_t handle;
+  /*cutensorHandle_t handle;
   cutensorInit(&handle);
 
   // Create Tensor Descriptors
@@ -315,31 +315,3 @@ int main(int argc, char** argv)
 
   return 0; 
 } 
-
-/*#include <stdlib.h>
-#include <stdio.h>
-
-#include <cuda_runtime.h>
-#include <cutensor.h>
-
-int main(int argc, char** argv)
-{
-  // Host element type definition
-  typedef float floatTypeA;
-  typedef float floatTypeB;
-  typedef float floatTypeC;
-  typedef float floatTypeCompute;
-
-  // CUDA types
-  cudaDataType_t typeA = CUDA_R_32F;
-  cudaDataType_t typeB = CUDA_R_32F;
-  cudaDataType_t typeC = CUDA_R_32F;
-  cutensorComputeType_t typeCompute = CUTENSOR_COMPUTE_32F;
-
-  floatTypeCompute alpha = (floatTypeCompute)1.1f;
-  floatTypeCompute beta  = (floatTypeCompute)0.9f;
-
-  printf("Include headers and define data types\n");
-
-  return 0;
-}*/
