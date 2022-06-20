a91474f08c32080ae28c7196c9a7c193100de6c7
nishqa <nishqasharmagradapps@gmail.com>
2022-06-20 03:38:39 -0700

    adding cuda files

--- helloworld.cu
+++ helloworld.cu
@@ -22,6 +22,7 @@ int main()
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
  
 // CUDA kernel. Each thread takes care of one element of c
 __global__ void vecAdd(double *a, double *b, double *c, int n)
